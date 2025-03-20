74e1809e4805b9a09d30a92c00017531375b6ef7
EvdISolo <Sagalakov1998ss@mail.ru>
2025-03-20 21:43:48 +0300

    Add files via upload

---
+++ ccwisp.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ccwisp {
+
+    public static void main(String[] args) {
+
+
+         //Задание №1
+System.out.println("Evdokim Sagalakov");
+        //Задание №2
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Введите Доход:");
+
+                int  income= sc.nextInt();
+
+                Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите Расход:");
+
+        int spending = sc2.nextInt();
+int remains=income-spending;
+                System.out.println("Итого (руб) \n"+remains);
+
+            }
+        }
+
+
+
+
+
